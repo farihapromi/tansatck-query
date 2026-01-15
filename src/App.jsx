4dc7438eda954649c65ea0303be072911afc5e5f
Fariha Promi <farihapromi2611@gmail.com>
2026-01-15 20:40:56 +0600

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// Create a router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/trad',
+        element: <FetchOld />,
+      },
+      {
+        path: '/rq',
+        element: <FetchRQ />,
+      },
+      {
+        path: '/rq/:id',
+        element: <FetchIndv />,
+      },
+      {
+        path: '/infinite',
+        element: <InfiniteScroll />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
   return <h1>Tanstack Query</h1>;
 };
