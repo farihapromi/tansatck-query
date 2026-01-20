c6b39cf9ca95798480b564cbccaa96a9e1ee4711
Fariha Promi <farihapromi2611@gmail.com>
2026-01-20 19:15:32 +0600

    Add fetchOld data

--- src/Pages/FetchOld.jsx
+++ src/Pages/FetchOld.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchPosts } from '../API/api';
 
 const FetchOld = () => {
+  const [posts, setPosts] = useState([]);
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      console.log(res);
+      res.status === 200 ? setPosts(res.data) : [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+  useEffect(() => {
+    getPostsData();
+  }, []);
   return (
     <div>
-      <h1>This is fetchold</h1>
+      <ul className='section-accordion'>
+        {posts?.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
