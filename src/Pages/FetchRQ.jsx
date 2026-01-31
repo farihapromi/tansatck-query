8b9a3ece1f7ce8324cf79daf2957cace26980542
Fariha Promi <farihapromi2611@gmail.com>
2026-01-31 18:13:13 +0600

    Use react query

--- src/Pages/FetchRQ.jsx
+++ src/Pages/FetchRQ.jsx
@@ -1,6 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { fetchPosts } from '../API/api';
 
 const FetchRQ = () => {
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      console.log(res);
+      res.status === 200 ? res.data : [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+  const { data } = useQuery({
+    queryyKey: ['posts'],
+    queryFn: getPostsData(),
+  });
+
   return (
     <div>
       <h1>This is fethrq</h1>
